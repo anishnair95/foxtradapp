c60eb659aa1f51f6602d6daf2f48f6daadc078a3
anishnair95 <anishnair95@gmail.com>
2021-03-21 14:06:00 +0530

    initial commit

---
+++ src/datab.js
@@ -0,0 +1,142 @@
+
+function getData(){
+
+    
+const fs=require('fs');
+
+
+const access_data=JSON.parse(fs.readFileSync('./src/cred.json','utf-8'));
+const a_token=access_data.access_token;
+
+
+
+var KiteTicker = require("kiteconnect").KiteTicker;
+var ticker = new KiteTicker({
+	api_key: "6zfi2amoxjco04yo",
+	access_token:a_token
+});
+
+
+ticker.connect();
+ticker.on("ticks", onTicks);
+ticker.on("connect", subscribe);
+
+
+const companyData = {
+    256265: { name: "NIFTY 50" ,
+              high:0,
+              low:0,
+              status:"",
+              stoploss:"NULL"
+             },
+
+    738561:{name: "RELIANCE",
+              high:0,
+              low:0,
+              status:"",
+              stoploss:"NULL"
+            },
+    779521: { name: "SBIN",
+              high:0,
+              low:0,
+              status:"",
+              stoploss:"NULL"
+             },
+    895745: { name: "TATASTEEL",
+              high:0,
+              low:0,
+              status:"",
+              stoploss:"NULL"
+             },
+    2813441:{name:"RADICO",
+              high:0,
+              low:0,
+              status:"",
+              stoploss:"NULL"
+            },
+
+    7401729:{name:"ROSSELLIND",
+              high:0,
+              low:0,
+              status:"",
+              stoploss:"NULL"
+            }
+
+};
+
+var items = [256265,738561, 779521,895745,2813441,7401729];
+
+//data getting in onticks
+ 
+   
+
+function onTicks(ticks) {
+
+
+ 
+
+    for (let i = 0; i < ticks.length; i++) {
+        var instrumentToken = ticks[i].instrument_token;
+
+        var high = ticks[i].ohlc.high;
+        var low = ticks[i].ohlc.low;
+        var ltp = ticks[i].last_price;
+
+        companyData[instrumentToken].high = high;
+        companyData[instrumentToken].low = low;
+        companyData[instrumentToken].lastTradePrice = ltp;
+
+        if (ltp > high) {
+            // console.log("buy");
+
+            companyData[instrumentToken].status = "buy";
+            companyData[instrumentToken].stoploss = low;
+        } else if (ltp < low) {
+            // console.log("sell");
+            companyData[instrumentToken].status = "sell";
+            companyData[instrumentToken].stoploss = high;
+        } else {
+            // console.log("stable");
+            companyData[instrumentToken].status = "Hold";
+            companyData[instrumentToken].stoploss = "NULL";
+        }
+        
+    }
+
+    data = [
+        {
+            Nifty: companyData[items[0]],
+            Reliance: companyData[items[1]],
+            Sbi: companyData[items[2]],
+            Tata:companyData[items[3]],
+            Radico:companyData[items[4]],
+            Rossellind:companyData[items[5]]
+
+        }
+
+    ];
+
+    fs.writeFileSync('./src/stock.json',JSON.stringify(data),'utf-8');
+
+    // console.log(data);
+  }
+
+
+
+   function subscribe() {
+
+	ticker.subscribe(items);
+	ticker.setMode(ticker.modeFull, items);
+   }
+
+  
+
+
+}
+
+module.exports=getData;
+
+
+
+
+
